1b4fa2311fcd478c4dc02d9f7ddf2a9b95bb10e1
lakshchhabra12 <laksh1363.becse24@chitkara.edu.in>
2025-01-28 00:26:41 +0530

    Solution to Input Two Integers and Print Their Sum

--- Input Two Integers and Print Their Sum@@6788d5012db25efec946f93d/code.c
+++ Input Two Integers and Print Their Sum@@6788d5012db25efec946f93d/code.c
@@ -1,10 +1,9 @@
-#include <iostream>
-using namespace std;
-int main(){
-    int a,b;
-    cout<<"enter two numbers: ";
-    cin>>a>>b;
-    int sum;
-    sum=a+b;
-    cout<<"sum of two numbers: "<<sum<<endl;
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%d ",&a);
+    scanf("%d",&b);
+    c = a + b;
+    printf("Sum: %d",c);
+    return 0;
 }
